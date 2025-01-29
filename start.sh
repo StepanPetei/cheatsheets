8bb8ac16b49a76df95bdc4975161ea69e805d17e
StepanPetei <stepan1906@gmail.com>
2025-01-29 22:38:38 +0100

    Fixed syntax errors

--- start.sh
+++ start.sh
@@ -10,41 +10,43 @@ declare -a apps=("HTTP" "HTTPS" "Apache" "Nginx" "OpenSSH")
 
 #Aqui podras declarar la lista de paquetes que te pareceran útiles para
 #empezar el trabajo con el sistema como con el servidor
-declare -a packages=("gdebi" "neofetch" "net-tools" "htop" "git" "xrdp" "nginx" "apache2" "openssh-client" "openssh-server" "curl" "code" "software-properties-common" "apt-transport-https" "wget")
+declare -a packages=("gdebi" "neofetch" "net-tools" "htop" "git" "xrdp" "nginx" "apache2" "openssh-client" "openssh-server" "curl")
 
 #Aqui podrás declarar los servicios que hace falta iniciar
 declare -a services=("sshd" "apache2")
 
+if [[ $EUID -ne 0 ]]; then
+    echo "Este script debe ejecutarse como root o con sudo."
+    exit 1
+fi
+
 apt update && apt upgrade && apt dist-upgrade
 #Primer paso: instalación de paquetes declarados en array "packages"
-for i in "${packages[@]}"
-do
-    if ($packages(i)=="code")
-    do
-        snap install $packages(i)
-    done
-    apt install $packages(i)
+echo "Instalando paquetes..."
+for package in "${packages[@]}"; do
+    apt install -y $package
 done
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-if (node -v <= 20)
-do
+# Verificar versión de Node.js e instalar si es necesario
+if [[ $(node -v | cut -d'.' -f1 | tr -d 'v') -le 20 ]]; then
     nvm install 22
-done
+fi
 apt update && apt upgrade && apt dist-upgrade
 
 #Segundo paso: inicialización del cortafuegos y
 #apertura de los puertos TCP clave que se han declarado en array "tcp_ports"
 ufw start
 ufw enable
-for i in "${tcp_ports[@]}"
-do
-    ufw allow from any to any port $tcp_ports(i)
+for port in "${tcp_ports[@]}"; do
+    ufw allow "$port"/tcp
 done
 #Habilitamos excepciones para apps en cortafuegos
 for i in "${apps[@]}"
 do
     ufw allow from any to any $apps(i)
 done
+
+echo "Configuración del firewall completada."
 
 #Agregamos usuarios para protocolos con cifrado al grupo
 adduser xrdp ssl-cert
