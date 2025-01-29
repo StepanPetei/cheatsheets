5610603251657ed7042c5358d4558cdb3ea6cf54
StepanPetei <stepan1906@gmail.com>
2025-01-30 00:00:44 +0100

    Fix

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
-apt update && apt upgrade && apt dist-upgrade
+declare -a tcp_ports=(21 22 53 57 80 123 443 1000 1645 1646 3389 5432 5433 10000)
+for port in "${tcp_ports[@]}"; do
+    ufw allow "$port"/tcp
+done
