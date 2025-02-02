69bf2fda5861c78ac913355078fb6fb50574f87a
Stepan Petei <48558745+StepanPetei@users.noreply.github.com>
2025-02-02 04:11:02 +0100

    Added installing of Google Chrome stable via apt

--- start.sh
+++ start.sh
@@ -36,6 +36,14 @@ fi
 if [[ $(node -v | cut -d'.' -f1 | tr -d 'v') -le $20 ]]; then
     nvm install 22
 fi
+
+curl -O https://packages.cloud.google.com/apt/doc/apt-key.gpg && sudo apt-key add apt-key.gpg
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' 
+
+# Install
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
 apt update && apt upgrade && apt dist-upgrade
 
 #Segundo paso: inicialización del cortafuegos y
