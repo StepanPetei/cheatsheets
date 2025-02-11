693f5151dc4516d1da51037cf1f691c90ccd41a2
StepanPetei <stepan1906@gmail.com>
2025-02-11 22:48:27 +0100

    Fixed nodejsupdate.sh

--- nodejsupdate.sh
+++ nodejsupdate.sh
@@ -1,5 +1,9 @@
-if [[ $(node -v | cut -d'.' -f1 | tr -d 'v') -le $20 ]]; then
-    nvm install 22
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
-npm install -g npm@latest
+#reinicia el terminal para seguir
+
+#if [[ $(node -v | cut -d'.' -f1 | tr -d 'v') -le $20 ]]; then
+#    nvm install 22
+#fi
+
+#npm install -g npm@latest
